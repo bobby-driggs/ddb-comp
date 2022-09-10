c14030de5ad5613724dd107223f2a369796bc447
Bobby Driggs <bobbydriggs@gmail.com>
2022-09-10 14:02:33 -0700

    Add munching util

---
+++ utils/ConvertPacks/Models/FoundryWorld.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertPacks.Models
+{
+
+	public class FoundryWorld
+	{
+		public string title { get; set; }
+		public string id { get; set; }
+		public string system { get; set; }
+		public string description { get; set; }
+		public string coreVersion { get; set; }
+		public FoundryWorldCompatibility compatibility { get; set; }
+		public string systemVersion { get; set; }
+		public FoundryWorldPack[] packs { get; set; }
+	}
+
+	public class FoundryWorldCompatibility
+	{
+		public string minimum { get; set; }
+		public string verified { get; set; }
+		public string maximum { get; set; }
+	}
+
+	public class FoundryWorldPack
+	{
+		public string type { get; set; }
+		public string label { get; set; }
+		public string name { get; set; }
+		public string path { get; set; }
+		public string system { get; set; }
+		public bool _private { get; set; }
+		public FoundryWorldFlags flags { get; set; }
+	}
+
+	public class FoundryWorldFlags
+	{
+	}
+
+}
---
+++ utils/ConvertPacks/Program.cs
@@ -0,0 +1,55 @@
+﻿using ConvertPacks.Models;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+var worldDirectory = @"\\moxie\moxie-nas\foundry-data\Data\worlds\ddb-comp";
+var outputDirectory = @"output";
+
+Directory.CreateDirectory(outputDirectory);
+
+var worldJsonPath = Path.Combine(worldDirectory, "world.json");
+var worldJson = File.ReadAllText(worldJsonPath);
+
+var worldModule = JsonSerializer.Deserialize<FoundryWorld>(worldJson);
+
+var worldId = worldModule.id;
+
+var replacementRegex = new Regex($"ddb-{worldId}-(.+)");
+
+var replacementInfo = worldModule.packs
+	.Select(uu => {
+
+		var match = replacementRegex.Match(uu.name);
+		var pack = match.Groups[1].Value;
+
+		return new
+		{
+			Regex = new Regex(uu.name),
+			ReplacementString = $"{pack}-ddb",
+		};
+	})
+	.ToList();
+
+foreach (var pack in worldModule.packs)
+{
+	Console.WriteLine($"Processing {pack.name}");
+
+	var packDbFilePath = Path.Combine(worldDirectory, pack.path);
+	var packData = File.ReadAllText(packDbFilePath);
+
+	var newWorldData = packData.Replace("@Compendium[world.", $"@Compendium[{worldId}.");
+
+	foreach (var info in replacementInfo)
+	{
+		newWorldData = info.Regex.Replace(newWorldData, info.ReplacementString);
+	}
+
+	var packMatch = replacementRegex.Match(pack.name);
+	var newPackName = packMatch.Groups[1].Value;
+
+	File.WriteAllText($"./{outputDirectory}/{newPackName}-ddb.db", newWorldData);
+}
+
+
+
+
